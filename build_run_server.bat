d9b2305c95851dc256bb1fb2c23deded746e46f8
deca <git@deca.gg>
2022-08-28 14:28:51 -0700

    🙈 Clean up untracked files

--- build_run_server.bat
+++
@@ -1,6 +0,0 @@
-dotnet build
-xcopy .\bin\Debug\netstandard2.1\LeadAHorseToWater.dll L:\SteamLibrary\steamapps\common\VRisingDedicatedServer\BepInEx\plugins /y
-
-pushd L:\SteamLibrary\steamapps\common\VRisingDedicatedServer\
-start_server_example.bat
-popd
